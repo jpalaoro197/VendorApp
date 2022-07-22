3f761184b2228d96e6005d008bc6ed803f44b034
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-22 10:47:49 -0700

    add order class and corresponding code for test to pass.

---
+++ VendorApp/Models/Order.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace VendorApp.Models
+{
+  public class Order
+  {
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
