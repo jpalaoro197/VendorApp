f0e412a96535628146c7d6a76bb4d0b5108f4342
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-22 10:56:43 -0700

    add test to ensure get description works

--- VendorApp.Tests/ModelTests/OrderTests.cs
+++ VendorApp.Tests/ModelTests/OrderTests.cs
@@ -19,5 +19,14 @@ namespace VendorApp.Tests
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
